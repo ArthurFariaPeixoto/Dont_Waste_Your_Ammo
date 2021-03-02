92cb0bc9eb7f81b453e11ecc26e92f5592fb98a7
Arthur Faria Peixoto <arthurfpeixoto@gmail.com>
2021-03-01 22:19:27 -0300

    Update Game.java

--- Game 02/src/com/arthur/main/Game.java
+++ Game 02/src/com/arthur/main/Game.java
@@ -25,6 +25,7 @@ public class Game extends Canvas implements Runnable, KeyListener {
     public static final int HEIGHT = 180;
     public static final int SCALE = 3;
 
+    private int Cur_level = 1, max_level = 4;
     private BufferedImage image;
     private BufferedImage bullet;
 
@@ -38,9 +39,16 @@ public class Game extends Canvas implements Runnable, KeyListener {
     public static Random rand;
     public static UI ui;
 
+    public static String gameState = "menu";
+    public static boolean gameover = true;
+    public static int framesgameover = 0;
+    private boolean restart = false;
+
+    public Menu menu;
 
 
-    public Game() {
+    public Game(){
+        Sound.music.loop();
         rand = new Random();
         addKeyListener(this);
         setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
@@ -58,10 +66,11 @@ public class Game extends Canvas implements Runnable, KeyListener {
         entities.add(player);
         bullet = spritesheet.getSprite(96, 16, 16, 16);
 
-
         /*Iniciando mundo*/
-        world = new World("/map.png");
+        world = new World("/level1.png");
         /* *** */
+
+        menu = new Menu();
 
     }
 
@@ -96,12 +105,44 @@ public class Game extends Canvas implements Runnable, KeyListener {
     }
 
     public void tick() {
-        for (int i = 0; i < entities.size(); i++) {
-            Entitie e = entities.get(i);
-            e.tick();
+        if(gameState == "normal") {
+            restart=false;
+            for (int i = 0; i < entities.size(); i++) {
+                Entitie e = entities.get(i);
+                e.tick();
+            }
+            for (int i = 0; i < bullets.size(); i++) {
+                bullets.get(i).tick();
+            }
+            if (enemies.size() == 0) {
+                Cur_level++;
+                if (Cur_level > max_level) {
+                    Cur_level = 1;
+                }
+                String newWorld = "level" + Cur_level + ".png";
+                World.Restart(newWorld);
+            }
+        }else if (gameState == "Game over"){
+            framesgameover++;
+            if(framesgameover == 30){
+                framesgameover = 0;
+                if(gameover){
+                    gameover = false;
+                }else{
+                    gameover = true;
+                }
+            }
+        }else if (gameState == "menu"){
+            menu.tick();
         }
-        for(int i = 0; i<bullets.size(); i++){
-            bullets.get(i).tick();
+        if (restart && gameState == "Game over"){
+            Sound.menu.play();
+            restart = false;
+            gameState = "normal";
+            Cur_level = 1;
+        String newWorld = "level" + Cur_level + ".png";
+        World.Restart(newWorld);
+
         }
     }
 
@@ -136,6 +177,20 @@ public class Game extends Canvas implements Runnable, KeyListener {
         g.setColor(Color.black);
         g.setFont(new Font("arial", Font.BOLD, 16));
         g.drawString("x "+player.bullet, 730, 33);
+        if(gameState == "Game over"){
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setColor(new Color(0,0,0,170));
+            g2.fillRect(0, 0, WIDTH*SCALE, HEIGHT*SCALE);
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("arial", Font.BOLD, 40));
+            g.drawString("Game Over!", (WIDTH*SCALE)/2 - 100, (HEIGHT*SCALE)/2);
+            if(gameover) {
+                g.setFont(new Font("arial", Font.BOLD, 15));
+                g.drawString("Press ENTER to restart.", (WIDTH * SCALE) / 2 - 70, (HEIGHT * SCALE) / 2 + 35);
+            }
+        }else if(gameState == "menu"){
+            menu.render(g);
+        }
         bs.show();
     }
 
@@ -175,33 +230,67 @@ public class Game extends Canvas implements Runnable, KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if(gameState == "normal"){
             player.right = true;
+            Sound.move.loop();
+            }
         } else if (e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT) {
-            player.left = true;
+            if(gameState == "normal") {
+                player.left = true;
+                Sound.move.loop();
+            }
+
         }
         if (e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP) {
-            player.up = true;
+            if(gameState == "normal") {
+                player.up = true;
+                Sound.move.loop();
+            }
+            if(gameState == "menu"){
+                menu.up = true;
+            }
         } else if (e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN) {
-            player.down = true;
+            if(gameState == "normal"){
+                player.down = true;
+                Sound.move.loop();
+            }
+            if(gameState == "menu"){
+                menu.down = true;
+            }
         }
         if(e.getKeyCode() == KeyEvent.VK_SPACE){
             player.shoot = true;
         }
+        if(e.getKeyCode() == KeyEvent.VK_ENTER){
+            restart = true;
+            if(gameState == "menu"){
+                menu.enter = true;
+            }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+            gameState = "menu";
+            menu.pause = true;
+        }
+
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         if(e.getKeyCode() == KeyEvent.VK_D || e.getKeyCode() == KeyEvent.VK_RIGHT){
             player.right = false;
+            Sound.move.stop();
         }
         else if(e.getKeyCode() == KeyEvent.VK_A || e.getKeyCode() == KeyEvent.VK_LEFT){
             player.left = false;
+            Sound.move.stop();
         }
         if(e.getKeyCode() == KeyEvent.VK_W || e.getKeyCode() == KeyEvent.VK_UP){
             player.up = false;
+            Sound.move.stop();
         }
         else if(e.getKeyCode() == KeyEvent.VK_S || e.getKeyCode() == KeyEvent.VK_DOWN){
             player.down = false;
+            Sound.move.stop();
         }
     }
 }
